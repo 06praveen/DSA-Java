c8bcc285e67bd4aefceee7b4ab37c3beeafc0060
Praveen Tiwari <Praveentiwari070615@gmail.com>
2026-06-10 23:50:42 +0530

    Solved a DSA queestion based on prefix & suffix
    Added a comment indicating the solution is based on prefix and suffix.

---
+++ 8-2574LeftRightDifference.java
@@ -0,0 +1,24 @@
+//Solved a DSA queestion based on prefix & suffix
+class Solution {
+    public int[] leftRightDifference(int[] arr) {
+        int n=arr.length;
+        int[] left= new int[n];
+        int[] right= new int[n];
+
+        left[0]=0;
+        right[n-1]=0;
+        
+        for(int i=1;i<n;i++){
+            left[i] = left[i-1]+arr[i-1];
+        }
+
+        for(int i=n-2;i>=0;i--){
+            right[i] = right[i+1]+arr[i+1];
+        }
+
+        for(int i=0;i<n;i++){
+            arr[i]=Math.abs(left[i]-right[i]);
+        }
+        return arr;
+    }
+}
